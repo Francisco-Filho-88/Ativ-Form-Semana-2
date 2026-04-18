9d0104d51478ec80ae9fafd401a89a545028ec3c
Francisco-Filho-88 <nascimento.francisco@pucpr.edu.br>
2026-04-17 21:35:04 -0300

    test: atualiza testes para sistema de elevador

--- test_app.py
+++ test_app.py
@@ -1,5 +1,26 @@
-from app import saudacao
+from app import Elevador
 
 
-def test_saudacao():
-    assert saudacao("João") == "Olá, João!"
+def test_subir():
+    elevador = Elevador()
+    assert elevador.subir() == 2
+
+
+def test_descer_no_primeiro_andar():
+    elevador = Elevador()
+    assert elevador.descer() == 1
+
+
+def test_ir_para_andar_valido():
+    elevador = Elevador()
+    assert elevador.ir_para(6) == 6
+
+
+def test_nao_ultrapassa_ultimo_andar():
+    elevador = Elevador(6)
+    assert elevador.subir() == 6
+
+
+def test_nao_desce_abaixo_primeiro_andar():
+    elevador = Elevador(1)
+    assert elevador.descer() == 1
